8125c75863ce5f0f99b70c9d6594691980bc9e71
s-batish <shivanibatish@btinternet.com>
2023-07-25 16:18:43 +0000

    Edit url for add_lesson

--- lessons/urls.py
+++ lessons/urls.py
@@ -3,7 +3,7 @@ from .views import AddLessons, OurClassesView, EditLesson, DeleteLesson
 
 
 urlpatterns = [
-    path('add_lessons/', AddLessons.as_view(), name='add_lessons'),
+    path('add_lesson/', AddLessons.as_view(), name='add_lesson'),
     path('our_classes/', OurClassesView.as_view(), name='our_classes'),
     path('edit_lesson/<slug:pk>/', EditLesson.as_view(), name='edit_lesson'),
     path('delete_lesson/<slug:pk>/', DeleteLesson.as_view(),
