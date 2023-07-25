bf1780cfc70f6b95ae7f2380e7b57c190bf9fd87
s-batish <shivanibatish@btinternet.com>
2023-07-25 11:28:35 +0000

    Edit url for creating lessons

--- lessons/urls.py
+++ lessons/urls.py
@@ -3,6 +3,6 @@ from .views import AddLessons, OurClassesView
 
 
 urlpatterns = [
-    path('', AddLessons.as_view(), name='add_lessons'),
+    path('add_lessons/', AddLessons.as_view(), name='add_lessons'),
     path('our_classes/', OurClassesView.as_view(), name='our_classes'),
 ]
