0901ca5c3dabf476cb74b8aeb4e3c8f9b9e26b78
s-batish <shivanibatish@btinternet.com>
2023-07-25 16:16:10 +0000

    Add edit lessons functionality

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import AddLessons, OurClassesView, DeleteLesson
+from .views import AddLessons, OurClassesView, EditLesson, DeleteLesson
 
 
 urlpatterns = [
     path('add_lessons/', AddLessons.as_view(), name='add_lessons'),
     path('our_classes/', OurClassesView.as_view(), name='our_classes'),
-    path('delete_lesson/<slug:pk>/', DeleteLesson.as_view(), name='delete_lesson')
+    path('edit_lesson/<slug:pk>/', EditLesson.as_view(), name='edit_lesson'),
+    path('delete_lesson/<slug:pk>/', DeleteLesson.as_view(),
+         name='delete_lesson'),
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView, DeleteView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Lessons
 from .forms import AddLessonsForm
@@ -28,6 +28,19 @@ class AddLessons(LoginRequiredMixin, CreateView):
         """
         form.instance.created_by = self.request.user
         return super(AddLessons, self).form_valid(form)
+
+
+class EditLesson(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    View for editing a lesson
+    """
+    template_name = 'lessons/edit_lessons.html'
+    model = Lessons
+    form_class = AddLessonsForm
+    success_url = '/lessons/our_classes'
+
+    def test_func(self):
+        return self.request.user.is_staff
 
 
 class DeleteLesson(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
