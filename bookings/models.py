dcc7995b284fedf6c66586a7f601888132aa3e1f
s-batish <shivanibatish@btinternet.com>
2023-07-27 12:25:23 +0000

    Add booking model and admin

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Choice fields
+LEVEL = (
+    ("beginner", "Beginner"),
+    ("intermediate", "Intermediate"),
+    ("advanced", "Advanced")
+)
+
+DAY = (
+    (1, "Monday"),
+    (2, "Tuesday"),
+    (3, "Wednesday"),
+    (4, "Thursday"),
+    (5, "Friday"),
+    (6, "Saturday"),
+    (7, "Sunday"),
+)
+
+TIME = (
+    (1, "09:00-11:00"),
+    (2, "12:00-14:00"),
+    (3, "15:00-17:00"),
+    (4, "18:00-20:00")
+)
+
+
+class Booking(models.Model):
+    """
+    Model to allow user to make a booking
+    """
+    client = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="client")
+    level = models.CharField(max_length=15, choices=LEVEL, default="beginner")
+    day = models.IntegerField(choices=DAY, default=1)
+    time = models.IntegerField(choices=TIME, default=1)
+
+    class Meta:
+        ordering = ['day']
+
+    def __str__(self):
+        return str(self.level)
