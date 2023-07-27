c030cb33e9d1ab054c2183d515fbbc394d4d7e9e
s-batish <shivanibatish@btinternet.com>
2023-07-27 08:47:05 +0000

    Edit allowed hosts in settings.py

--- tangotennis/settings.py
+++ tangotennis/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-sbatish-tangotennis-kmy5ygsl2f8.ws-eu102.gitpod.io',
+ALLOWED_HOSTS = ['8000-sbatish-tangotennis-998e1b9j40i.ws-eu102.gitpod.io',
                  'tango-tennis-9fd40e1a7462.herokuapp.com']
 
 
