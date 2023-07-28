ca615461ba0d7b744470a02ea7f2f1d4ddb8aea5
s-batish <shivanibatish@btinternet.com>
2023-07-28 16:58:43 +0000

    Edit views.py to only show user's bookings

--- bookings/views.py
+++ bookings/views.py
@@ -2,6 +2,7 @@ from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Booking
 from .forms import BookingForm
+from django.shortcuts import render
 
 
 class BookingsList(ListView):
@@ -11,6 +12,18 @@ class BookingsList(ListView):
     template_name = 'bookings/manage_bookings.html'
     model = Booking
     context_object_name = 'bookings'
+
+    # Ensures logged in users can only see their own bookings but staff can
+    # see all bookings
+    # Code from:
+    # https://www.youtube.com/watch?v=q2GGBThrgmA&list=PL_6Ho1hjJirn8WbY4xfVUAlcn51E4cSbY&index=5
+    def get_queryset(self, *args, **kwargs):
+        if self.request.user.is_staff:
+            booking_list = Booking.objects.all()
+            return booking_list
+        else:
+            booking_list = Booking.objects.filter(client=self.request.user)
+            return booking_list
 
 
 class AddBooking(LoginRequiredMixin, CreateView):
