b65173d9f436f3d588ccfa8790ae4b6a394b2bcc
s-batish <shivanibatish@btinternet.com>
2023-08-02 14:50:32 +0000

    Add review model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'name',
+        'created_on',
+        'body'
+    )
+    list_filter = ('created_on',)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Review(models.Model):
+    """
+    Model to add reviews about the lessons
+    """
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user")
+    name = models.CharField(max_length=80)
+    created_on = models.DateTimeField(auto_now=True)
+    body = models.TextField(max_length=350, null=False, blank=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
