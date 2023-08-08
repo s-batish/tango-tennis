67afd411674f8cec7bc5b0245a4cbc07e042ac04
s-batish <shivanibatish@btinternet.com>
2023-08-08 17:33:27 +0000

    Add home views tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Review
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 400)
+        self.assertTemplateUsed(response, 'home/index.html')
+
+
+class TestRedirectViews(TestCase):
+    
+    def test_add_review(self):
+        response = self.client.get('/add_review/')
+        self.assertEqual(response.status_code, 302)
