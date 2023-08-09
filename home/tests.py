cb9196f9a3690e259a90251f5fc5d02f6437d4e3
s-batish <shivanibatish@btinternet.com>
2023-08-09 09:14:14 +0000

    Add home redirect views tests

--- home/tests.py
+++ home/tests.py
@@ -11,7 +11,16 @@ class TestViews(TestCase):
 
 
 class TestRedirectViews(TestCase):
-    
+    # Tests when a user is not logged in
+
     def test_add_review(self):
         response = self.client.get('/add_review/')
+        self.assertEqual(response.status_code, 302)
+    
+    def test_edit_review(self):
+        response = self.client.get('/edit_review/1/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_review(self):
+        response = self.client.get('/delete_review/1/')
         self.assertEqual(response.status_code, 302)
